a9cdcec608dee96b8c6c7c2493743f960313012e
Paulina Szczepanska <paulina.szczepanska122@gmail.com>
2019-04-04 20:06:52 +0200

    szukanie po PESEL

--- crm/src/main/java/oby_na_trzy/crm/repository/KlienciRepository.java
+++ crm/src/main/java/oby_na_trzy/crm/repository/KlienciRepository.java
@@ -2,6 +2,16 @@ package oby_na_trzy.crm.repository;
 
 import oby_na_trzy.crm.model.Klienci;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface KlienciRepository extends JpaRepository<Klienci, Integer> {
+
+
+    @Query(value = "SELECT * FROM klienci WHERE pesel = :peselValue",
+            nativeQuery = true)
+    List<Klienci> findAllByPesel(@Param("peselValue") String pesel);
+
 }
--- crm/src/main/java/oby_na_trzy/crm/resource/KlienciResource.java
+++ crm/src/main/java/oby_na_trzy/crm/resource/KlienciResource.java
@@ -2,11 +2,17 @@ package oby_na_trzy.crm.resource;
 
 import oby_na_trzy.crm.model.Klienci;
 import oby_na_trzy.crm.repository.KlienciRepository;
+import org.hibernate.annotations.Subselect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.Column;
 import java.util.List;
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.findAll;
 
 @RestController
 @RequestMapping(value = "/klienci")
@@ -40,11 +46,23 @@ public class KlienciResource {
         return mv;
     }
 
-    @GetMapping(value = "/search/{id}")
-    public ModelAndView doSearch(@PathVariable("id") int id){
+    @GetMapping(value = "/searchbyid/{id}")
+    public ModelAndView doSearchById(@PathVariable("id") int id){
 
         ModelAndView mv = new ModelAndView("tabela");
         mv.addObject("lists", klienciRepository.findOne(id));
         return mv;
     }
+
+    @GetMapping(value = "/searchbypesel/{pesel}")
+
+    public ModelAndView doSearchByPesel(@PathVariable("pesel") String pesel){
+
+        ModelAndView mv = new ModelAndView("tabela");
+        mv.addObject("lists", klienciRepository.findAllByPesel(pesel));
+        return mv;
+    }
+
+
+
 }
