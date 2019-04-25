4ed36e71660cc0f32f9ac9dbd6fe8451c05d5568
maciejkozikowski <maciej.kozik94@gmail.com>
2019-04-25 10:26:16 +0200

    model telefony

---
+++ crm/src/main/java/oby_na_trzy/crm/model/Telefony.java
@@ -0,0 +1,54 @@
+package oby_na_trzy.crm.model;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "telefony")
+public class Telefony {
+    @Id //pole numer jest identyfikatorem w tabeli
+    @Column(name = "numer") //pole odpowiada kolumnie numer w tabeli
+    private String numer;
+
+    @Column(name = "postpaid")//pole odpowiada kolumnie pospaid w tabeli
+    private boolean postpaid;
+
+    @Column(name = "stan_konta")
+    private float stan_konta;
+
+    @Column(name = "darmowy_rozmowy")
+    private boolean darmowe_rozmowy;
+
+    @Column(name = "darmowe_smsy")
+    private boolean darmowe_smsy;
+
+    @Column(name = "darmowy_internet")
+    private boolean darmowy_internet;
+
+    @Column(name = "IdKlienta")
+    private int IdKlienta;
+
+    public Telefony() {}
+
+    public String getNumer() {return numer;}
+
+    public void setNumer(String numer) {this.numer = numer;}
+
+    public boolean getPostpaid(){return postpaid;}
+
+    public void setPostpaid(boolean postpaid) {this.postpaid = postpaid;}
+
+    public float getStan_konta(){return stan_konta;};
+
+    public void setStan_konta(float stan_konta){this.stan_konta = stan_konta;}
+
+    public boolean getDarmowe_rozmowy(){return darmowe_rozmowy;}
+
+    public void setDarmowe_rozmowy(boolean darmowe_rozmowy){this.darmowe_rozmowy = darmowe_rozmowy;}
+
+    public boolean getDarmowe_smsy(){return darmowe_smsy;}
+
+    public void setDarmowe_smsy(boolean darmowe_smsy){this.darmowe_smsy = darmowe_smsy;}
+
+    public boolean getDarmowy_internet(){return darmowy_internet;}
+
+    public void setDarmowy_internet(boolean darmowy_internet){this.darmowy_internet = darmowy_internet;}
+}
