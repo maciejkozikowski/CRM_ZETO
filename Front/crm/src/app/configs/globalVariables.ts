ed6541b60fbd4489f9775c40b80be511da11f559
Paulina Szczepanska <paulina.szczepanska122@gmail.com>
2019-04-27 12:07:09 +0200

    Zmiana adresu v2

--- Front/crm/src/app/configs/globalVariables.ts
+++ Front/crm/src/app/configs/globalVariables.ts
@@ -1,2 +1,2 @@
 //export const serverURL = 'http://localhost:8080/';
-export const serverURL = 'http://ec2-18-221-42-207.us-east-2.compute.amazonaws.com:8080/';
+export const serverURL = 'http://ec2-3-16-161-75.us-east-2.compute.amazonaws.com:8080/';
