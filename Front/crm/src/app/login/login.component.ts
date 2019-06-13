568e305b0a315c4e77569ca568518a712d68b3b2
Paulina Szczepanska <paulina.szczepanska122@gmail.com>
2019-06-13 19:39:12 +0200

    przekierowanie do home po zalogowaniu

--- Front/crm/src/app/login/login.component.ts
+++ Front/crm/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { TokenStorageService } from '../auth/token-storage.service';
 import { AuthLoginInfo } from '../auth/login-info';
+import { Router } from '@angular/router';
  
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
   roles: string[] = [];
   private loginInfo: AuthLoginInfo;
  
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private router: Router) { }
  
   ngOnInit() {
     if (this.tokenStorage.getToken()) {
@@ -53,6 +54,6 @@ export class LoginComponent implements OnInit {
   }
  
   reloadPage() {
-    window.location.reload();
+    this.router.navigate(['/home']);
   }
 }
